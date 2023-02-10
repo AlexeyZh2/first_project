e8d1e9850590a3793bee8efe269e78cff4aaf417
Alexey <alexeyzhukov@MacBook-Pro-Alexey.local>
2023-02-10 17:09:06 +0300

    Страничка о проектах

--- src/views/AboutProjectVew.vue
+++ src/views/AboutProjectVew.vue
@@ -1,8 +1,49 @@
 <template>
-    <h2>О проектах</h2>
+    <img src="../assets/image4.jpg"/>
+    <div class="main_container">
+        <h2>О проектах</h2>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+            Ipsam accusamus cupiditate laborum. Quos, dolores?
+            Iure saepe aspernatur obcaecati, eveniet hic repellat
+            voluptates suscipit! Alias placeat totam sapiente delectus
+            commodi accusantium, vitae eius tempora sit veniam, consequatur
+            maiores et mollitia id laudantium ullam eos ea quis! Ullam voluptatum
+            cupiditate maxime? Qui ea voluptate itaque delectus molestias enim
+            ducimus dolorem laborum repellendus assumenda consequuntur, voluptatem
+            cum magni ipsa? Aspernatur cumque rem vel similique eveniet minus incidunt?
+            Dolorum facilis porro assumenda repudiandae. Dolorum aliquid,
+            officiis quae laborum sunt natus suscipit consequatur non beatae
+            illo velit incidunt autem unde officia, nulla commodi repellendus at
+            laboriosam praesentium. Quibusdam quisquam nesciunt error repellat
+            expedita sequi est vitae adipisci. Mollitia soluta est ratione, reiciendis,
+            iusto ipsa cupiditate fuga voluptatum maxime quibusdam laudantium
+            molestias velit et minima. At nam, architecto adipisci eaque vitae
+            laborum ducimus illo voluptatem aperiam illum id reiciendis ipsum,
+            quisquam ratione sit laudantium temporibus dolores?</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aperiam
+            fugit in, accusantium voluptatem, nihil corrupti obcaecati dolor
+            consectetur voluptate nostrum recusandae. Cum animi ad optio ipsa
+            vel dicta mollitia harum. Distinctio dolor consectetur dolores amet,
+            ipsam maxime mollitia molestiae aliquam cupiditate officiis quis aspernatur
+            corrupti minima repudiandae, nisi accusamus tempore vero, ducimus adipisci
+            deleniti. Fuga eius numquam culpa eaque! Commodi veniam architecto
+            sapiente quibusdam adipisci, error unde sed quasi? Distinctio amet tempora
+            excepturi qui dignissimos repellat quae, reprehenderit sequi, soluta est
+            aspernatur iure consectetur dicta exercitationem necessitatibus mollitia,
+            error laboriosam nesciunt ipsa. Nihil minus laudantium necessitatibus
+            amet reprehenderit quaerat excepturi.</p>
+    </div>
 </template>
 <script>
 export default {
-  
+
 }
 </script>
+<style>
+img {
+  width: 80vw;
+  height: 20vh;
+  object-fit: cover;
+  margin: 0 auto;
+}
+</style>
